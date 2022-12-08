165d51bd891694c0578f1e28e3acfb2a53895e9d
Michael Cummins <michaelcummins@Michaels-MacBook-Air.local>
2022-12-08 14:42:59 +0100

    Finished ex4 - uploading 5

---
+++ ex4_classification/cnn_network.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torchvision.transforms.autoaugment import AutoAugmentPolicy
+
+
+class CNN(nn.Module):
+    """Convolutional Neural Network.
+    
+    We provide a simple network with a Conv layer, followed by pooling,
+    and a fully connected layer. Modify this to test different architectures,
+    and hyperparameters, i.e. different number of layers, kernel size, feature
+    dimensions etc.
+
+    See https://pytorch.org/docs/stable/nn.html for a list of different layers
+    in PyTorch.
+    """
+
+    def __init__(self):
+        """Initialize layers."""
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(6,10,5)
+        self.fc1 = nn.Linear(810, 400)
+        self.fc2 = nn.Linear(400,200)
+        self.fc3 = nn.Linear(200,10)
+        self.drop = nn.Dropout(p=0.5)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        """Forward pass of network."""
+        x = self.pool(self.activation(self.conv1(x)))
+        x = self.pool(self.activation(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.activation(x)
+        x = self.drop(x)
+        x = self.fc3(x)
+        return x
+
+def get_transforms_train():
+    """Return the transformations applied to images during training.
+    
+    See https://pytorch.org/vision/stable/transforms.html for a full list of 
+    available transforms.
+    """
+    N,M=2,10
+    transform = transforms.Compose(
+        [
+            transforms.ColorJitter(brightness=0, contrast=0, saturation=0, hue=0),
+            transforms.ToPILImage(),
+            transforms.RandomHorizontalFlip(),
+            # transforms.RandomCrop(32, 4),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                    std=[0.5, 0.5, 0.5]),
+            transforms.RandomErasing(),
+        ]
+    )
+    return transform
+
+
+def get_transforms_val():
+    """Return the transformations applied to images during validation.
+
+    Note: You do not need to change this function 
+    """
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5,0.5,0.5],
+                                    std=[0.5,0.5,0.5])
+        ]
+    )
+    return transform
+
+
+def get_loss_function():
+    """Return the loss function to use during training. We use
+       the Cross-Entropy loss for now.
+    
+    See https://pytorch.org/docs/stable/nn.html#loss-functions.
+    """
+    return nn.CrossEntropyLoss()
+
+
+def get_optimizer(network, lr, momentum=0.9):
+    """Return the optimizer to use during training.
+    
+    network specifies the PyTorch model.
+
+    See https://pytorch.org/docs/stable/optim.html#how-to-use-an-optimizer.
+    """
+
+    # The fist parameter here specifies the list of parameters that are
+    # learnt. In our case, we want to learn all the parameters in the network
+    return optim.Adam(network.parameters(), lr=lr, weight_decay=0.001)
